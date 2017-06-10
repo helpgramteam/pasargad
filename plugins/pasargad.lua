88afb2c6e5779222cfd00d281d6ebeb6cb9d322a
helpgramteam <amir1ali2robat@gmail.com>
2017-06-10 17:07:33 +0430

    amir

--- plugins/pasargad.lua
+++ plugins/pasargad.lua
@@ -1,7 +1,7 @@
 ﻿local function amir(msg, matches)
   if matches[1] == "pasargad" or "پاسارگاد" then
       if is_sudo(msg) then
-       return "@pasargadcute"
+       return "انلاینم عزیزم و حواسم به همه چی هست"
     end
   end	
 end  
